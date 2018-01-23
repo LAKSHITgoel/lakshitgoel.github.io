e1545e252f54fd6d4bdea8cdbd348ce64fc0e70c
LAKSHITgoel <glaskhit0@gmail.com>
2018-01-23 13:35:22 +0530

    udate quote machine

--- quotemachine/function.js
+++ quotemachine/function.js
@@ -21,7 +21,26 @@ function quote(){
 $(document).ready(function(){
     curQuote='';
     curAuthor='';
-   $("#new_quote").on("click",quote());
+   $("#new_quote").on("click",function() {
+       $.ajax({
+           headers: {
+               "X-Mashape-Key": "jKdq5Vn0lsmshVNnjhHMo9RQhcMRp1VLHGHjsnz8oqiqkYQ9jK",
+               Accept: "application/json",
+               "Content-Type": "application/x-www-form-urlencoded"
+           },
+           url:"https://andruxnet-random-famous-quotes.p.mashape.com/?cat=",
+           success: function(r)
+           {
+               if(typeof r == 'string')
+               {
+                   r=JSON.parse(r);
+               }
+               curAuthor=r.author;
+               curQuote=r.quote;
+               $("#quote").html("<p>"+r.quote+"</p>" + "<br><p>  &minus; " + r.author + "</p>");
+           }
+       });
+   });
    $("#tweet-quote").on("click",function(){
        $("#tweet-quote").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=LAKSHITgoel&text=' + encodeURIComponent('"' + curQuote + '" ' +"-"+curAuthor));
    });
