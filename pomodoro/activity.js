e2d03806774681dfce0ddfea31d76ec528b7b93e
LAKSHITgoel <glaskhit0@gmail.com>
2018-01-24 21:28:07 +0530

    pomodoro clock done!

--- pomodoro/activity.js
+++ pomodoro/activity.js
@@ -1,12 +1,14 @@
 //---------------global varibles------------------
-var startFlag = false;
+var sessionFlag = false;
+var breakFlag = false;
+var interval;
 //-----------------update break length------------
 function updateBL(btnid) {
   var str = $("#breakTime");
   // if minus is pressed then decrement only if break length is greater than zero
   if (btnid=="BLminus")
   {
-    if (str.text()=="0") {return null;}
+    if (str.text()=="1") {return;}
     else
     {
       var strVal = Number(str.text().trim());
@@ -28,7 +30,7 @@ function updateSL(btnid) {
   // if minus is pressed then decrement only if session length is greater than zero
   if (btnid=="SLminus")
   {
-    if (str.text()=="0") return null;
+    if (str.text()=="1") return null;
     else
     {
       var strVal = Number(str.text().trim());
@@ -43,37 +45,94 @@ function updateSL(btnid) {
     strVal++;
     str.text(strVal);
   }
-  updateTimer(btnid);
+  updateTimer();
 }
-//------------------update timer--------------------------------
-function updateTimer(btnid){
-  var str = $("#timer");
-  if (btnid=="SLminus"){
-    if (str.text()=="0") {return null;}
-    else {
-      var strVal = Number(str.text().trim());
-      strVal--;
-      str.text(strVal);
-    }
+//------------------update timer when session length is updated--------------------------------
+function updateTimer() {
+    var str = $("#timer");
+    var str2= $("#sessionTime").text();
+    str.text(str2+" : 00");
+}
+//-------------------clock simulator-----------------------------
+function countDown(time) {              // let time = 25
+    var minutes = Number(time);
+    interval = setInterval(function(){
+      arr = $("#timer").text().split(":");
+      var seconds = Number(arr[1]);
+      seconds--;
+      if (minutes < 0 && seconds < 0 ) return;
+      else if (seconds <= 0 && minutes !=0){
+        minutes--;
+        seconds = 59;
+      }
+      else if (seconds < 10 && String(seconds).length !=2) {
+          seconds = "0"+seconds;
+          if ( minutes== 0 && seconds == 00) {
+            clearInterval(interval);
+              if(sessionFlag ==true&& breakFlag==false){
+                sessionFlag = false;
+                startBreak();
+              }
+              else if(sessionFlag==false&&breakFlag==true){
+                breakFlag = false;
+                startSession();
+              }
+          }
+      }
+      $("#timer").text(minutes+" : "+seconds);
+      },1000);
+}
+//-------------------break-------------------------------
+function startBreak(){
+  breakFlag = true;
+  $("#currentRoutine").text("Break!");
+  var break_time = Number($("#breakTime").text());
+  countDown(break_time);
+}
+//-------------------session-----------------------------
+function startSession(){
+  sessionFlag = true;
+  $("#currentRoutine").text("Session");
+  var arr = $("#sessionTime").text();
+  var session_time = Number(arr);
+  countDown(session_time);
+}
+//-------------------start-------------------------------
+function start() {
+  if (sessionFlag==false && breakFlag==false){
+    sessionFlag = true;
+    startSession();
   }
-  else {
-    var strVal = Number(str.text().trim());
-    strVal++;
-    str.text(strVal);
+  else if (sessionFlag == false && breakFlag == true){
+    sessionFlag = true;
+    breakFlag = false;
+    startBreak();
   }
+$(".start").attr('disabled','true');
+$(".plus").attr('disabled','true');
+$(".minus").attr('disabled','true');
 }
-//-------------------start or stop-------------------------------
-function StartStop(){
-
+//---------------------reset-----------------------------
+function reset(){
+  $(".start").removeAttr('disabled');
+  $(".minus").removeAttr('disabled');
+  $(".plus").removeAttr('disabled');
+  $("#breakTime").text("5");
+  $("#sessionTime").text("25");
+  $("#timer").text("25 : 00");
+  $("#currentRoutine").text("Session");
+  sessionFlag = false;
+  breakFlag = false;
+  clearInterval(interval);
 }
-
 //--------------------------------------------------
 $(document).ready(function(){
   $("button").on("click",function(){
     var btnid = this.id;
     if(btnid=="BLminus"||btnid=="BLplus") updateBL(btnid);
     else if (btnid=="SLminus"||btnid=="SLplus") updateSL(btnid);
-    else if (btnid=="control") StartStop();
+    else if (btnid=="reset") reset();
+    else if (btnid=="start") start();
 
   });
 });
